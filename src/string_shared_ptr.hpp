400ecddf84e836a9d342ffa51e9fa126f166d78e
jannikac <jannikac@mailbox.org>
2022-01-20 21:35:44 +0100

    Header aktualisiert

--- src/string_shared_ptr.hpp
+++ src/string_shared_ptr.hpp
@@ -1,16 +1,21 @@
 #ifndef STRING_SHARED_PTR_HPP
 #define STRING_SHARED_PTR_HPP
+#include "rc.hpp"
 #include "string.hpp"
+#include <cstdlib>
 
 class string_shared_ptr {
 private:
+  rc *reference;
+  string *strptr;
+
 public:
   // Konstruktor
   string_shared_ptr(string *ptr);
   // Copy-Konstruktor
-  string_shared_ptr(const string_shared_ptr &a);
+  string_shared_ptr(const string_shared_ptr &other);
   void reset();
-  string_shared_ptr operator->();
+  string *operator->();
   ~string_shared_ptr();
 };
 #endif
