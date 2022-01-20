4150d280f774bd4397937f7a14ab57f1c4d9e74a
jannikac <jannikac@mailbox.org>
2022-01-20 21:36:01 +0100

    Implementierung erstellt

--- src/string_shared_ptr.cpp
+++ src/string_shared_ptr.cpp
@@ -1,1 +1,28 @@
-#include "string.hpp"
+#include "string_shared_ptr.hpp"
+
+string_shared_ptr::string_shared_ptr(string *ptr) {
+  reference = new rc();
+  strptr = ptr;
+  // Referenz erhöhen
+  reference->increment();
+}
+
+string_shared_ptr::string_shared_ptr(const string_shared_ptr &other) {
+  // refCount des neuen Zeigers auf Refcount des alten setzen
+  reference = other.reference;
+  strptr = other.strptr;
+  // Neue Referenz, daher refCount erhöhen
+  reference->increment();
+};
+
+string_shared_ptr::~string_shared_ptr() {
+  reference->decrement();
+  if (reference->getCount() == 0) {
+    delete strptr;
+    delete reference;
+  }
+}
+
+void string_shared_ptr::reset() { strptr = nullptr; }
+
+string *string_shared_ptr::operator->() { return strptr; }
