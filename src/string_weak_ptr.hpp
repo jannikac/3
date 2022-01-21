7bb8ceb6bf67a64922fe83bd0fe82aeb3e9aabb4
jannikac <jannikac@mailbox.org>
2022-01-21 15:53:22 +0100

    Added implementation for weak ptr

--- src/string_weak_ptr.hpp
+++ src/string_weak_ptr.hpp
@@ -1,13 +1,17 @@
 #ifndef STRING_WEAK_PTR_HPP
 #define STRING_WEAK_PTR_HPP
 #include "string.hpp"
+#include "string_shared_ptr.hpp"
 
 class string_weak_ptr {
 private:
+  string *strptr;
+  rc *reference;
+
 public:
-  // Konstruktor
-  string_weak_ptr(const string &ref);
-  void lock();
+  // Konstruktor aus einem string_shared_ptr
+  string_weak_ptr(const string_shared_ptr &other);
+  string_shared_ptr lock();
   ~string_weak_ptr();
 };
 #endif
