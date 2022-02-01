ee15306b2866f305278eb58f0356df0ca14b85a6
jannikac <jannikac@mailbox.org>
2022-02-01 13:33:45 +0100

    Updated dependency

--- src/string.cpp
+++ src/string.cpp
@@ -4,83 +4,74 @@
 #include <stdexcept>
 #include <string.h>
 
+// Erstellt eine `string`-Instanz mit der Zeichenfolge ""
 string::string() {
   // Weist Heap-Speicher mit der Größe von einem char zu
   str = (char *)malloc(sizeof(char));
 }
 
+// Erstellt eine `string` Instanz aus einem Zeiger auf ein `char` Array.
 string::string(const char *inputString) {
   // inputString auf nullptr überprüfen, damit strlen() keinen segfault erzeugt.
-  // Bei einem nullptr wird eine exception geworfen
-  if (inputString) {
-    // größe für internen str festlegen
-    str = (char *)malloc(sizeof(char) * strlen(inputString));
-
-    // c-string von der Eingabe in den internen str kopieren
-    strcpy(str, inputString);
-  } else {
-    // Exception werfen
+  // Bei einem nullptr wird eine Exception geworfen
+  if (!inputString)
     throw std::invalid_argument("recieved nullptr");
-  }
+
+  // größe für internen str festlegen
+  str = (char *)malloc(sizeof(char) * strlen(inputString));
+
+  // c-string von der Eingabe in den internen str kopieren
+  strcpy(str, inputString);
 }
 
+// Fügt ein `char` Array an einen den internen `str` an.
 void string::append(const char *inputString) {
   // inputString auf nullptr überprüfen, damit strlen() keinen segfault erzeugt.
   // Bei einem nullptr wird eine exception geworfen
-  if (inputString) {
-    // Größe des internen str um Länge des inputStrings erweitern
-    str = (char *)realloc(str,
-                          sizeof(char) * (strlen(inputString) + strlen(str)));
-    // inputString an bestehenden str anhängen
-    strcat(str, inputString);
-  } else {
-    // Exception werfen
+  if (!inputString)
     throw std::invalid_argument("recieved nullptr");
-  }
+
+  // Größe des internen str um Länge des inputStrings erweitern
+  str =
+      (char *)realloc(str, sizeof(char) * (strlen(inputString) + strlen(str)));
+  // inputString an bestehenden str anhängen
+  strcat(str, inputString);
 }
 
-char *string::data() {
-  // Gibt den internen str zurück.
-  // Kann keinen nullptr zurückgeben, da bei eingabe eines nullptrs eine
-  // exception geworfen wird
-  return str;
-}
+// Gibt den internen `str` zurück.
+// Kann keinen `nullptr` zurückgeben, da bei eingabe eines `nullptrs` eine
+// Exception geworfen wird.
+char *string::data() { return str; }
 
+// Findet den ersten `char` im internen `str`, welcher mit `c` übereinstimmt und
+// gibt den entsprechenden Index zurück. Wird char nicht gefunden, wird `-1`
+// zurück gegeben.
 int string::find(char c) {
-  // Findet den ersten char, im internen str und gibt den entsprechenden Index
-  // zurück. Wird char nicht gefunden, wird -1 zurück gegeben.
-  char *res;
-  int index;
-
-  // Gibt Zeiger auf gefundenen str zurück und speichert ihn als res
-  res = strchr(str, c);
+  // Gibt Zeiger auf gefundenen char zurück und speichert ihn als res
+  char *res = strchr(str, c);
+  // Wenn res nullptr zurückgibt, wurde kein char gefunden, also -1 als index
+  if (!res)
+    return -1;
 
   // Wenn res keinen nullptr zurückgibt, wurde char gefunden
-  if (res) {
-    // Adresse des gefundenen Zeigers - Adresse des Strings ergibt den Index
-    index = (int)(res - str);
-    return index;
-  } else {
-    // Falls char nicht gefunden wurde, also res = nullptr, gib als index -1
-    // zurück
-    return -1;
-  }
+  // Adresse des gefundenen Zeigers - Adresse des Strings = Index
+  int index = (int)(res - str);
+  return index;
 }
 
-void string::print() {
-  // Zeichenkette wird mit cout ausgegeben
-  std::cout << str << std::endl;
-}
+// Interner `str` wird mit `std::cout` ausgegeben.
+void string::print() { std::cout << str << std::endl; }
 
+// `str` wird mit 0 gefüllt und dann auf die Länge 1 zurückgesetzt, damit
+// dieser mit `string::append` wiederverwendet werden kann.
 void string::clear() {
-  // str wird mit 0 gefüllt und dann auf die Länge 1 zurückgesetzt, damit
-  // dieser wiederverwendet werden kann.
   // str mit 0 füllen.
   memset(str, 0, strlen(str));
   // Größe zurücksetzen
-  str = (char *)malloc(sizeof(char));
+  str = (char *)realloc(str, sizeof(char));
 }
 
+// Destruktor
 string::~string() {
   // Speicher an der Stelle des str pointers freigeben.
   // Clear aufzurufen reicht nicht, da der Speicher dadurch nicht freigegeben
